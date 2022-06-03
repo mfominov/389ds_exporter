ad2dc581dd8ff0cfabbcb9d07b36dae1b6b37c3f
Maksim Fominov <maxfominov@gmail.com>
2022-06-03 11:10:18 +0300

    test lint rule skip 2

--- ldap/util.go
+++ ldap/util.go
@@ -23,7 +23,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-//lint:file-ignore ST1005
+//lint:file-ignore ST1005 Allow capitalized strings for errors
 
 package ldap
 
